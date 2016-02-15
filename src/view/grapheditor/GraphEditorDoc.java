d33029530ab7b59f1aae911405016fa4e345002b
Rizhi_KotE <d1mqn1ch@mail.ru>
2016-02-15 21:56:30 +0300

    full integration

---
+++ src/view/grapheditor/GraphEditorDoc.java
@@ -0,0 +1,27 @@
+package view.grapheditor;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.*;
+
+public class GraphEditorDoc extends JPanel {
+
+	JPanel comp = this;
+
+	public GraphEditorDoc() {
+		super();
+		setSize(200, 200);
+		//JLabel label = new JLabel("кнопка нажата");
+		//comp.add(label);
+		//addNode();
+		setBackground(new Color(255));
+		setVisible(true);
+	}
+
+
+	public void addNode() {
+
+	}
+}
